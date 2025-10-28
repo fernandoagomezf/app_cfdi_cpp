6c77e1e515c87478dc5d19c5ca5aa7928a2f12cd
Fernando Arturo Gómez Flores <fernando.gomez@blendwerk.com.mx>
2025-10-27 18:50:48 -0600

    refactor: remoción de fmt y uso de print en su lugar

--- src/program.cpp
+++ src/program.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
 #include <fstream>
 #include <filesystem>
-#include <fmt/core.h>
+#include <print>
 #include "xmlreader.hpp"
 
-using std::cin;
 using std::ostringstream;
 using std::string;
 using std::ifstream;
-using fmt::print;
+using std::print;
 using cfdi::XmlNode;
 using cfdi::XmlNodeType;
 using cfdi::XmlReader;
@@ -16,12 +15,11 @@ using cfdi::XmlReader;
 int main() {
     print("\n=== XmlReader Demo ===\n");
 
-    string fileName { "../data/d0e7ed17-e3ec-41ca-9cf8-61b01da88fc5.xml" };
+    string fileName { "data/d0e7ed17-e3ec-41ca-9cf8-61b01da88fc5.xml" };
     ifstream file(fileName);
     if (!file.is_open()) {
-        print("\n *** CFDI file {} not found.", fileName);
-        print("\n {}", std::filesystem::current_path().string());
-        cin.get();
+        print("\n *** CFDI file {0} not found.", fileName);
+        print("\n {0}", std::filesystem::current_path().string());
         return -1;
     }
     
@@ -35,7 +33,7 @@ int main() {
         
         switch (node.nodeType) {
             case XmlNodeType::Element:
-                print("Element: {} (prefix: '{}', localName: '{}')", node.name, node.prefix, node.localName);
+                print("Element: {0} (prefix: '{1}', localName: '{2}')", node.name, node.prefix, node.localName);
                 if (node.isEmpty) {
                     print(" [Empty]");
                 }
@@ -46,18 +44,18 @@ int main() {
                         for (int i = 0; i < node.depth + 1; i++) {
                             print("  ");
                         }
-                        print("@{} = \"{}\"\n", name, value);
+                        print("@{0} = \"{1}\"\n", name, value);
                     }
                 }
                 break;
                 
             case XmlNodeType::EndElement:
-                print("EndElement: {}\n", node.name);
+                print("EndElement: {0}\n", node.name);
                 break;
 
             case XmlNodeType::Text:
                 if (!node.value.empty()) {
-                    print("Text: \"{}\"\n", node.value);
+                    print("Text: \"{0}\"\n", node.value);
                 }
                 break;
                 
